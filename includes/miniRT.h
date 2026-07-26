587028f324c341d5e15e10ab81e6ed811d0a0a53
Filipe Conde <filipecondep@gmail.com>
2026-07-25 21:36:45 -0300

    feat: cria arquivo main, arquivo de cabeçalhos miniRT e arquivo sample para testes iniciais do setup do projeto.

---
+++ includes/miniRT.h
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   miniRT.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fconde-p <fconde-p@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/07/25 20:43:25 by fconde-p          #+#    #+#             */
+/*   Updated: 2026/07/25 21:23:49 by fconde-p         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINIRT_H
+# define MINIRT_H
+
+// # include "../lib/libft/libft.h"
+// # include <unistd.h>
+// # include <fcntl.h>
+// # include <stdlib.h>
+
+# include "./lib/libft/libft.h"
+# include <sys/wait.h>
+# include <readline/readline.h>
+# include <readline/history.h>
+# include <unistd.h>
+# include <fcntl.h>
+# include <stdlib.h>
+# include <signal.h>
+
+int	sample_return(int hard_result);
+
+#endif
