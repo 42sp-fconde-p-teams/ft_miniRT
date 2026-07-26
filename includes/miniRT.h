4644c3c905eab37e8cc617cddbec36f4ae5c0954
Filipe Conde <filipecondep@gmail.com>
2026-07-25 21:50:06 -0300

    refactor: remove importações não usadas e mantém apenas as essenciais.

--- includes/miniRT.h
+++ includes/miniRT.h
@@ -6,26 +6,17 @@
 /*   By: fconde-p <fconde-p@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/25 20:43:25 by fconde-p          #+#    #+#             */
-/*   Updated: 2026/07/25 21:23:49 by fconde-p         ###   ########.fr       */
+/*   Updated: 2026/07/25 21:49:36 by fconde-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINIRT_H
 # define MINIRT_H
 
-// # include "../lib/libft/libft.h"
-// # include <unistd.h>
-// # include <fcntl.h>
-// # include <stdlib.h>
-
-# include "./lib/libft/libft.h"
-# include <sys/wait.h>
-# include <readline/readline.h>
-# include <readline/history.h>
+# include "../lib/libft/libft.h"
 # include <unistd.h>
 # include <fcntl.h>
 # include <stdlib.h>
-# include <signal.h>
 
 int	sample_return(int hard_result);
 
