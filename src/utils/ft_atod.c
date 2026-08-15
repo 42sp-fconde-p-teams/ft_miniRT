b44ee17c7d6384d81c8745664639fb2bd9c6647f
Filipe Conde <filipecondep@gmail.com>
2026-08-15 18:43:33 -0300

    feat: converte números com parte fracionária de string para double.

--- src/utils/ft_atod.c
+++ src/utils/ft_atod.c
@@ -6,68 +6,65 @@
 /*   By: fconde-p <fconde-p@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/14 20:15:11 by fconde-p          #+#    #+#             */
-/*   Updated: 2026/08/15 17:12:26 by fconde-p         ###   ########.fr       */
+/*   Updated: 2026/08/15 18:40:43 by fconde-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/miniRT.h"
 
-static double	gen_int_part(char *ptr, double acc)
+static double	gen_int_part(char **ptr, double acc)
 {
-	while (ft_isdigit(*ptr) > 0)
+	while (**ptr && ft_isdigit(**ptr) > 0)
 	{
-		acc = acc * 10.0 + (*ptr - '0');
-		ptr++;
+		acc = acc * 10.0 + (**ptr - '0');
+		(*ptr)++;
 	}
 	return (acc);
 }
 
-static double	gen_fractional_part(char *ptr, double acc)
+static double	gen_fractional_part(char **ptr, double acc)
 {
 	double	divisor;
 
 	divisor = 10.0;
-	while (ft_isdigit(*ptr) > 0)
+	while (**ptr && ft_isdigit(**ptr) > 0)
 	{
-		acc += (*ptr - '0') / divisor;
+		acc += (**ptr - '0') / divisor;
 		divisor *= 10.0;
-		ptr++;
+		(*ptr)++;
 	}
 	return (acc);
 }
 
-static double	check_sign(char *ptr, double *sign)
+static int	check_sign(char **ptr, double *sign)
 {
-	if (*ptr == '-' || *ptr == '+')
+	if (**ptr == '-' || **ptr == '+')
 	{
-		if (ft_isdigit(*(ptr + 1)) == 0)
-			return (0);
-		if (*ptr == '-')
+		if (**ptr == '-')
 			*sign = -1.0;
-		ptr++;
+		(*ptr)++;
 	}
 	return (1);
 }
 
-double	ft_atod(const char *nptr)
+double	ft_atod(char *nptr)
 {
 	double	sign;
 	double	acc;
-	double	*ptr_sign;
 
-	ptr_sign = &sign;
 	sign = 1.0;
 	acc = 0.0;
-	if (!check_sign((char *)nptr, ptr_sign))
+	if (!check_sign(&nptr, &sign))
 		return (0);
-	while (*nptr)
+	if (ft_isdigit(*nptr) > 0)
 	{
-		if (ft_isdigit(*nptr) > 0)
-		{
-			acc = gen_long((char *)nptr, acc);
-			return (acc * sign);
-		}
+		acc = gen_int_part(&nptr, acc);
+	}
+
+	if (*nptr == '.')
+	{
 		nptr++;
+		acc = gen_fractional_part(&nptr, acc);
 	}
 	return (acc * sign);
 }
