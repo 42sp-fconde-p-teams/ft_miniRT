dcfb752e65f2816073591d5a0f2095b8a3fc9989
Filipe Conde <fconde-p@student.42sp.org.br>
2026-08-20 19:12:52 -0300

    refactor: cria funções auxiliares para economia de linhas na função principal.

--- src/parser/reader/read_file.c
+++ src/parser/reader/read_file.c
@@ -3,14 +3,37 @@
 /*                                                        :::      ::::::::   */
 /*   read_file.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: fconde-p <fconde-p@student.42.fr>          +#+  +:+       +#+        */
+/*   By: fconde-p <fconde-p@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/12 13:03:49 by fconde-p          #+#    #+#             */
-/*   Updated: 2026/08/13 12:33:34 by fconde-p         ###   ########.fr       */
+/*   Updated: 2026/08/20 19:11:56 by fconde-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../../includes/miniRT.h"
+#include "../../../includes/miniRT.h"
+
+static int	deal_with_ext_err(char *file)
+{
+	if (check_extention(file) == EXIT_FAILURE)
+	{
+		printf("Error\nFile extention must be \".rt\"!\n");
+		return (EXIT_FAILURE);
+	}
+	return (EXIT_SUCCESS);
+}
+
+static int	deal_with_lines(char *line, int fd)
+{
+	while (line)
+	{
+		printf("%s\n", line);
+		free(line);
+		line = get_next_line(fd);
+	}
+	if (line)
+		free(line);
+	return (EXIT_SUCCESS);
+}
 
 int	read_file(char *file)
 {
@@ -19,19 +42,11 @@ int	read_file(char *file)
 
 	fd = 0;
 	line = NULL;
-	if (check_extention(file) == EXIT_FAILURE)
-	{
-		printf("Error\nFile extention must be \".rt\"!\n");
+	if (deal_with_ext_err(file) == EXIT_FAILURE)
 		return (EXIT_FAILURE);
-	}
 	fd = open(file, O_RDONLY);
 	line = get_next_line(fd);
-	while (line)
-	{
-		printf("%s\n", line);
-		free(line);
-		line = get_next_line(fd);
-	}
+	deal_with_lines(line, fd);
 	if (line)
 		free(line);
 	close(fd);
