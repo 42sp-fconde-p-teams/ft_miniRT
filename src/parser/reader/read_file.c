e2b0f1fa49e97662f08a826a38ce0df5316845b4
Filipe Conde <fconde-p@student.42sp.org.br>
2026-08-20 20:18:45 -0300

    fix: resolve leak de memória nas execuções de ft_split()..

--- src/parser/reader/read_file.c
+++ src/parser/reader/read_file.c
@@ -6,7 +6,7 @@
 /*   By: fconde-p <fconde-p@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/12 13:03:49 by fconde-p          #+#    #+#             */
-/*   Updated: 2026/08/20 19:49:33 by fconde-p         ###   ########.fr       */
+/*   Updated: 2026/08/20 20:17:33 by fconde-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,21 +22,39 @@ static int	deal_with_ext_err(char *file)
 	return (EXIT_SUCCESS);
 }
 
+static void	free_split(char **split)
+{
+	size_t	i;
+
+	if (!split)
+		return ;
+	i = 0;
+	while (split[i])
+	{
+		free(split[i]);
+		i++;
+	}
+	free(split);
+}
+
 static int	deal_with_lines(char *line, int fd)
 {
 	char	**split_line;
+	int		i;
 
 	split_line = NULL;
 	line = get_next_line(fd);
 	while (line)
 	{
+		i = 0;
 		split_line = ft_split(line, ' ');
 		printf("NBR OF ELEMENTS: %zu\n", count_split_elements(split_line));
-		while (*split_line)
+		while (split_line[i])
 		{
-			printf("%s\n", *split_line);
-			split_line++;
+			printf("%s\n", split_line[i]);
+			i++;
 		}
+		free_split(split_line);
 		free(line);
 		line = get_next_line(fd);
 	}
