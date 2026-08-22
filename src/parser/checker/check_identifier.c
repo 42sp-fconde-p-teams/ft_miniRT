2612b62f36d8ab88844cc68e1cce2603bc1e3441
Filipe Conde <filipecondep@gmail.com>
2026-08-22 18:39:00 -0300

    feat: valida com sucesso identificadores válidos.

--- src/parser/checker/check_identifier.c
+++ src/parser/checker/check_identifier.c
@@ -6,7 +6,7 @@
 /*   By: fconde-p <fconde-p@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/22 17:55:54 by fconde-p          #+#    #+#             */
-/*   Updated: 2026/08/22 18:07:22 by fconde-p         ###   ########.fr       */
+/*   Updated: 2026/08/22 18:38:22 by fconde-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,11 @@
 
 int	check_identifier(char **line)
 {
-	if (line[0][0] == 'A')
+	if (line[0][0] == 'A' || line[0][0] == 'C' || line[0][0] == 'L')
+		return (EXIT_SUCCESS);
+	else if ((line[0][0] == 's' && line[0][1] == 'p')
+		|| (line[0][0] == 'p' && line[0][1] == 'l')
+		|| (line[0][0] == 'c' && line[0][1] == 'y'))
 		return (EXIT_SUCCESS);
 	return (EXIT_FAILURE);
 }
