e15bfe8c2cffb4b81d562b83f8ec62d53c2d3322
thais.fer <thais.fer_@outlook.com>
2026-08-24 00:23:43 -0300

    fix: alteração dos retornos de sucesso e falha para manter padrão do projeto

--- src/engine/vector_utils.c
+++ src/engine/vector_utils.c
@@ -6,7 +6,7 @@
 /*   By: thfernan <thfernan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/18 05:50:10 by thfernan          #+#    #+#             */
-/*   Updated: 2026/08/18 05:51:13 by thfernan         ###   ########.fr       */
+/*   Updated: 2026/08/22 18:35:39 by thfernan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,10 @@
 int	vec3_almost_equal(t_vec3 a, t_vec3 b)
 {
 	if (fabs(a.x - b.x) >= EPSILON)
-		return (0);
+		return (EXIT_SUCCESS);
 	if (fabs(a.y - b.y) >= EPSILON)
-		return (0);
+		return (EXIT_SUCCESS);
 	if (fabs(a.z - b.z) >= EPSILON)
-		return (0);
-	return (1);
+		return (EXIT_SUCCESS);
+	return (EXIT_FAILURE);
 }
