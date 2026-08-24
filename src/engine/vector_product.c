5f41d0ec2ca2a104d22bcca28d9bb3fb1f94fa04
thais.fer <thais.fer_@outlook.com>
2026-08-24 00:22:12 -0300

    feat: adiciona vec3_dot para medir o quanto dois vetores apontam na mesma direção

--- src/engine/vector_product.c
+++ src/engine/vector_product.c
@@ -6,13 +6,13 @@
 /*   By: thfernan <thfernan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/18 05:51:50 by thfernan          #+#    #+#             */
-/*   Updated: 2026/08/18 06:20:51 by thfernan         ###   ########.fr       */
+/*   Updated: 2026/08/22 18:24:13 by thfernan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/miniRT.h"
 
-// 
+// Measures if two vectors point in the same direction, and how much.
 double	vec3_dot(t_vec3 a, t_vec3 b)
 {
 	double	product;
