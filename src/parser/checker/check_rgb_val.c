e5ec2aaa14ef6b2be56d83f25b29e5169ec502a8
Filipe Conde <filipecondep@gmail.com>
2026-08-25 13:03:22 -0300

    test: cria teste para checagem de valores RGB e ajusta função para comportamento adequado.

--- src/parser/checker/check_rgb_val.c
+++ src/parser/checker/check_rgb_val.c
@@ -6,7 +6,7 @@
 /*   By: fconde-p <fconde-p@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/25 00:02:55 by fconde-p          #+#    #+#             */
-/*   Updated: 2026/08/25 00:14:04 by fconde-p         ###   ########.fr       */
+/*   Updated: 2026/08/25 13:01:43 by fconde-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,17 @@
 int	check_rgb_val(char *rgb)
 {
 	char	**split_rgb;
+	int		i;
 
+	i = 0;
 	split_rgb = ft_split(rgb, ',');
+	while (rgb[i])
+	{
+		if (ft_isdigit(rgb[i]) == 0 && rgb[i] != ',')
+			return (EXIT_FAILURE);
+		i++;
+	}
+	
 	if (ft_count_split_elements(split_rgb) != 3)
 		return (EXIT_FAILURE);
 	if (((ft_atoi(split_rgb[0]) < 0) || (ft_atoi(split_rgb[0]) > 255))
