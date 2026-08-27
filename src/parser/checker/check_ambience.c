db1317d1b274436143871a7b93e40aa8e7460c03
Filipe Conde <filipecondep@gmail.com>
2026-08-27 16:03:37 -0300

    feat: check_ambience() agora valida também o range do número double no segundo parâmetro indicando taxa de iluminação.

--- src/parser/checker/check_ambience.c
+++ src/parser/checker/check_ambience.c
@@ -6,7 +6,7 @@
 /*   By: fconde-p <fconde-p@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/24 23:33:47 by fconde-p          #+#    #+#             */
-/*   Updated: 2026/08/27 15:30:10 by fconde-p         ###   ########.fr       */
+/*   Updated: 2026/08/27 16:00:43 by fconde-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,9 @@ int	check_ambience(char **line)
 {
 	if (ft_count_split_elements(line) != 3)
 		return (EXIT_FAILURE);
-	if	(ft_is_double(line[1]) == EXIT_FAILURE)
+	if (ft_is_double(line[1]) == EXIT_FAILURE)
+		return (EXIT_FAILURE);
+	if (check_num_double_in_range(0.0, 1.0, ft_atod(line[1])) == EXIT_FAILURE)
 		return (EXIT_FAILURE);
 	if (check_rgb_val(line[2]) == EXIT_FAILURE)
 		return (EXIT_FAILURE);
