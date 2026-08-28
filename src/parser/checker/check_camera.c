cc711657b9b87c1a729213a194f232e68f0ba838
Filipe Conde <filipecondep@gmail.com>
2026-08-28 14:31:36 -0300

    fix: limpa memória alocada em split de string.

--- src/parser/checker/check_camera.c
+++ src/parser/checker/check_camera.c
@@ -6,7 +6,7 @@
 /*   By: fconde-p <fconde-p@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/28 11:56:01 by fconde-p          #+#    #+#             */
-/*   Updated: 2026/08/28 12:00:33 by fconde-p         ###   ########.fr       */
+/*   Updated: 2026/08/28 14:28:27 by fconde-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,19 @@
 
 int	check_camera(char **line)
 {
+	char	**split_el;
+
+	split_el = ft_split(line[2], ',');
 	if (ft_count_split_elements(line) != 4)
 		return (EXIT_FAILURE);
+	if (check_coordinates(line[1]) == EXIT_FAILURE)
+		return (EXIT_FAILURE);
+	if (check_coordinates(line[2]) == EXIT_FAILURE)
+		return (EXIT_FAILURE);
+	if (check_num_double_in_range(-1.0, 1.0, ft_atod(split_el[0])) 
+		== EXIT_FAILURE)
+		return (EXIT_FAILURE);
+	if (split_el)
+		ft_free_array(split_el);
 	return (EXIT_SUCCESS);
 }
