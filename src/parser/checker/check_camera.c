fe686a8cfede79ce3e65bb464209258a730d47d9
Filipe Conde <filipecondep@gmail.com>
2026-08-28 15:08:02 -0300

    refactor: aplica norm42.

--- src/parser/checker/check_camera.c
+++ src/parser/checker/check_camera.c
@@ -6,7 +6,7 @@
 /*   By: fconde-p <fconde-p@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/28 11:56:01 by fconde-p          #+#    #+#             */
-/*   Updated: 2026/08/28 14:42:03 by fconde-p         ###   ########.fr       */
+/*   Updated: 2026/08/28 15:07:17 by fconde-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,10 +33,10 @@ int	check_camera(char **line)
 		return (EXIT_FAILURE);
 	}
 	if (check_double_in_range(-1.0, 1.0, ft_atod(split_el[0])) == EXIT_FAILURE)
-		{
-			ft_free_array(split_el);
-			return (EXIT_FAILURE);
-		}
+	{
+		ft_free_array(split_el);
+		return (EXIT_FAILURE);
+	}
 	ft_free_array(split_el);
 	return (EXIT_SUCCESS);
 }
--- src/parser/checker/check_rgb_val.c
+++ src/parser/checker/check_rgb_val.c
@@ -6,44 +6,35 @@
 /*   By: fconde-p <fconde-p@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/25 00:02:55 by fconde-p          #+#    #+#             */
-/*   Updated: 2026/08/28 14:59:48 by fconde-p         ###   ########.fr       */
+/*   Updated: 2026/08/28 15:07:23 by fconde-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../../includes/miniRT.h"
 
-// static int	loop_is_digit(char **rgb)
-// {
-// 	int	i;
+static int	loop_is_digit(char *rgb)
+{
+	int	i;
 
-// 	i = 0;
-// 	while (rgb[i])
-// 	{
-// 		if (ft_isdigit(rgb[i]) == 0 && rgb[i] != ',')
-// 		{
-// 			ft_free_array(rgb);
-// 			return (EXIT_FAILURE);
-// 		}
-// 		i++;
-// 	}
-// 	return (EXIT_SUCCESS);
-// }
+	i = 0;
+	while (rgb[i])
+	{
+		if (ft_isdigit(rgb[i]) == 0 && rgb[i] != ',')
+			return (EXIT_FAILURE);
+		i++;
+	}
+	return (EXIT_SUCCESS);
+}
 
 int	check_rgb_val(char *rgb)
 {
 	char	**split_rgb;
-	int		i;
 
-	i = 0;
 	split_rgb = ft_split(rgb, ',');
-	while (rgb[i])
+	if (loop_is_digit(rgb) == EXIT_FAILURE)
 	{
-		if (ft_isdigit(rgb[i]) == 0 && rgb[i] != ',')
-		{
-			ft_free_array(split_rgb);
-			return (EXIT_FAILURE);
-		}
-		i++;
+		ft_free_array(split_rgb);
+		return (EXIT_FAILURE);
 	}
 	if (ft_count_split_elements(split_rgb) != 3)
 	{
@@ -53,10 +44,10 @@ int	check_rgb_val(char *rgb)
 	if (((ft_atoi(split_rgb[0]) < 0) || (ft_atoi(split_rgb[0]) > 255))
 		|| ((ft_atoi(split_rgb[1]) < 0) || (ft_atoi(split_rgb[1]) > 255))
 		|| ((ft_atoi(split_rgb[2]) < 0) || (ft_atoi(split_rgb[2]) > 255)))
-		{
-			ft_free_array(split_rgb);
-			return (EXIT_FAILURE);
-		}
+	{
+		ft_free_array(split_rgb);
+		return (EXIT_FAILURE);
+	}
 	ft_free_array(split_rgb);
 	return (EXIT_SUCCESS);
 }
