f691a502f33e6df38a1459ffc4fe61bf29120dfc
Filipe Conde <filipecondep@gmail.com>
2026-08-30 20:55:51 -0300

    refactor: cria função auxiliar para checagem dos elementos únicos.

--- src/parser/checker/check_line.c
+++ src/parser/checker/check_line.c
@@ -6,16 +6,14 @@
 /*   By: fconde-p <fconde-p@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/20 22:02:16 by fconde-p          #+#    #+#             */
-/*   Updated: 2026/08/30 19:42:06 by fconde-p         ###   ########.fr       */
+/*   Updated: 2026/08/30 20:51:41 by fconde-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../../includes/miniRT.h"
 
-int	check_line(char **split_line)
+static int	check_unique_elements(char **split_line)
 {
-	if (check_identifier(split_line) == EXIT_SUCCESS)
-	{
 		if ((split_line[0][0] == 'A')
 			&& (check_ambience(split_line) == EXIT_SUCCESS))
 		{
@@ -34,6 +32,15 @@ int	check_line(char **split_line)
 			printf("LIGHT OK\n");
 			return (EXIT_SUCCESS);
 		}
-	}
+		else
+			return (EXIT_FAILURE);
+}
+
+int	check_line(char **split_line)
+{
+	if (check_unique_elements(split_line) == EXIT_SUCCESS)
+		return (EXIT_SUCCESS);
+	// else if (check_solid_elements(split_line) == EXIT_SUCCESS)
+	// 	return (EXIT_SUCCESS);
 	return (EXIT_FAILURE);
 }
