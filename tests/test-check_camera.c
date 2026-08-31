466e2933f8e796dacb32916aebe752658c9714ba
Filipe Conde <filipecondep@gmail.com>
2026-08-30 22:57:55 -0300

    test: cria testes de identificador para check_camera().

--- tests/test-check_camera.c
+++ tests/test-check_camera.c
@@ -11,6 +11,26 @@ int	should_succeed_for_valid_camera_line(void)
 		return (EXIT_FAILURE);
 }
 
+int	should_fail_for_lowercase_c(void)
+{
+	char	*line[] = {"c", "-50.0,0.0,20.0", "0.0,0.0,1.0", "70", NULL};
+
+	if (check_camera(line) == EXIT_FAILURE)
+		return (EXIT_SUCCESS);
+	else
+		return (EXIT_FAILURE);
+}
+
+int	should_fail_when_identifier_more_than_one_char(void)
+{
+	char	*line[] = {"CC", "-50.0,0.0,20.0", "0.0,0.0,1.0", "70", NULL};
+
+	if (check_camera(line) == EXIT_FAILURE)
+		return (EXIT_SUCCESS);
+	else
+		return (EXIT_FAILURE);
+}
+
 int	should_fail_for_orient_vec_above_one(void)
 {
 	char	*line[] = {"C", "-50.0,0.0,20.0", "0.0,0.0,1.1", "70", NULL};
@@ -58,4 +78,6 @@ int	main(void)
 	RUN_TEST(should_fail_for_orient_vec_below_minus_one);
 	RUN_TEST(should_fail_for_FOV_above_180);
 	RUN_TEST(should_fail_for_FOV_below_zero);
+	RUN_TEST(should_fail_for_lowercase_c);
+	RUN_TEST(should_fail_when_identifier_more_than_one_char);
 }
