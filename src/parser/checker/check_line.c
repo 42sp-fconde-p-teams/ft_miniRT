20ac64ef8c597964e0b63db0d088fb79670258df
Filipe Conde <filipecondep@gmail.com>
2026-08-30 22:43:34 -0300

    refactor: transfere resnponsabilidade de checar identificador para check_light().

--- src/parser/checker/check_line.c
+++ src/parser/checker/check_line.c
@@ -6,7 +6,7 @@
 /*   By: fconde-p <fconde-p@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/20 22:02:16 by fconde-p          #+#    #+#             */
-/*   Updated: 2026/08/30 22:27:07 by fconde-p         ###   ########.fr       */
+/*   Updated: 2026/08/30 22:41:11 by fconde-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,8 +24,7 @@ static int	check_unique_elements(char **split_line)
 			printf("CAMERA OK\n");
 			return (EXIT_SUCCESS);
 		}
-		else if ((split_line[0][0] == 'L')
-			&& (check_light(split_line) == EXIT_SUCCESS))
+		else if (check_light(split_line) == EXIT_SUCCESS)
 		{
 			printf("LIGHT OK\n");
 			return (EXIT_SUCCESS);
