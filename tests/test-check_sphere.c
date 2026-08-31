2740999ceadead674fb2f6ddb36cbed762861c90
Filipe Conde <filipecondep@gmail.com>
2026-08-30 23:03:36 -0300

    test: cria testes de identificador para check_sphere().

--- tests/test-check_sphere.c
+++ tests/test-check_sphere.c
@@ -11,7 +11,29 @@ int	should_succeed_for_valid_sphere_params(void)
 		return (EXIT_FAILURE);
 }
 
+int	should_fail_for_identifier_with_capital_char(void)
+{
+	char	*line[] = {"sP", "0.0,0.0,20.6", "12.6", "10,0,255", NULL};
+
+	if (check_sphere(line) == EXIT_FAILURE)
+		return (EXIT_SUCCESS);
+	else
+		return (EXIT_FAILURE);
+}
+
+int	should_fail_for_identifier_with_more_than_two_char(void)
+{
+	char	*line[] = {"ssp", "0.0,0.0,20.6", "12.6", "10,0,255", NULL};
+
+	if (check_sphere(line) == EXIT_FAILURE)
+		return (EXIT_SUCCESS);
+	else
+		return (EXIT_FAILURE);
+}
+
 int	main(void)
 {
 	RUN_TEST(should_succeed_for_valid_sphere_params);
+	RUN_TEST(should_fail_for_identifier_with_capital_char);
+	RUN_TEST(should_fail_for_identifier_with_more_than_two_char);
 }
