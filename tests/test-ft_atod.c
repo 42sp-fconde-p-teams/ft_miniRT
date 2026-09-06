94778f67824567ec57a1aeb0f0171b52015c5ac0
Filipe Conde <filipecondep@gmail.com>
2026-09-06 00:01:33 -0300

    test: adiciona testes para atod() converter int representado em ascii para double.

--- tests/test-ft_atod.c
+++ tests/test-ft_atod.c
@@ -67,6 +67,39 @@ int	should_convert_neg_nbr_zero_string_to_double(void)
 	return (EXIT_FAILURE);
 }
 
+int	should_convert_positive_int_string_to_double(void)
+{
+	double	d_nbr = 0.0;
+	char	str[3] = "42";
+
+	d_nbr = ft_atod(str);
+	if (d_nbr == 42.0)
+		return (EXIT_SUCCESS);
+	return (EXIT_FAILURE);
+}
+
+int	should_convert_negative_int_string_to_double(void)
+{
+	double	d_nbr = 0.0;
+	char	str[4] = "-42";
+
+	d_nbr = ft_atod(str);
+	if (d_nbr == -42.0)
+		return (EXIT_SUCCESS);
+	return (EXIT_FAILURE);
+}
+
+int	should_convert_zero_int_string_to_double(void)
+{
+	double	d_nbr = 0.0;
+	char	str[2] = "0";
+
+	d_nbr = ft_atod(str);
+	if (d_nbr == 0.0)
+		return (EXIT_SUCCESS);
+	return (EXIT_FAILURE);
+}
+
 int	main(void)
 {
 	RUN_TEST(should_convert_positive_nbr_string_to_double);
@@ -75,5 +108,8 @@ int	main(void)
 	RUN_TEST(should_convert_pos_nbr_starting_with_zero_string_to_double);
 	RUN_TEST(should_convert_pos_nbr_zero_string_to_double);
 	RUN_TEST(should_convert_neg_nbr_zero_string_to_double);
+	RUN_TEST(should_convert_positive_int_string_to_double);
+	RUN_TEST(should_convert_negative_int_string_to_double);
+	RUN_TEST(should_convert_zero_int_string_to_double);
 	return (0);
 }
