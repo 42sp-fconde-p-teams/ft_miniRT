3fd2fcb02302bbea33f66f94307b72645f7671d9
Filipe Conde <filipecondep@gmail.com>
2026-09-08 17:50:22 -0300

    refactor: remove impressões de debug do teste.

--- tests/test-set_ambience.c
+++ tests/test-set_ambience.c
@@ -12,14 +12,7 @@ int	should_set_valid_ambience_line(void)
 		|| scene.ambience.color.red != 100
 		|| scene.ambience.color.green != 150
 		|| scene.ambience.color.blue != 200)
-	{
-		printf("LIGHTNING  %f\n", scene.ambience.lighting);
-		printf("RED  %d\n", scene.ambience.color.red);
-		printf("GREEN  %d\n", scene.ambience.color.green);
-		printf("BLUE  %d\n", scene.ambience.color.blue);
-		printf("COMP  %d\n", ft_double_equals(scene.ambience.lighting, 0.2));
 		return (EXIT_FAILURE);
-	}
 	else
 		return (EXIT_SUCCESS);
 }
