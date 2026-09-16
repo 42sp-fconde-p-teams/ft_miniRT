cc76f77421300c2582b13231a7d0ccb9a1d6307f
Filipe Conde <fconde-p@student.42sp.org.br>
2026-09-16 20:21:06 -0300

    feat: cria limpeza de lista de elementos spheres operando elemento único e lista de spheres.

--- src/utils/free_scene.c
+++ src/utils/free_scene.c
@@ -3,18 +3,33 @@
 /*                                                        :::      ::::::::   */
 /*   free_scene.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: fconde-p <fconde-p@student.42.fr>          +#+  +:+       +#+        */
+/*   By: fconde-p <fconde-p@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/09/13 21:24:02 by fconde-p          #+#    #+#             */
-/*   Updated: 2026/09/13 21:30:30 by fconde-p         ###   ########.fr       */
+/*   Updated: 2026/09/16 20:05:47 by fconde-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/miniRT.h"
 
+static void	free_sphere(t_scene *scene)
+{
+	t_sphere	*temp;
+
+	temp = NULL;
+	while (scene->sphere != NULL)
+	{
+		temp = scene->sphere;
+		scene->sphere = scene->sphere->next;
+		free(temp);
+	}
+}
+
 int	free_scene(t_scene	*scene)
 {
-	if (scene->sphere != NULL)
-		free(scene->sphere);
+	if (scene != NULL)
+	{
+		free_sphere(scene);
+	}
 	return (EXIT_SUCCESS);
 }
