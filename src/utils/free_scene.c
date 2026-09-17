563deaea94ccc7607a984df6185226028a6a2fee
Filipe Conde <fconde-p@student.42sp.org.br>
2026-09-16 21:31:18 -0300

    feat: cria limpeza de memória para plain.

--- src/utils/free_scene.c
+++ src/utils/free_scene.c
@@ -6,7 +6,7 @@
 /*   By: fconde-p <fconde-p@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/09/13 21:24:02 by fconde-p          #+#    #+#             */
-/*   Updated: 2026/09/16 20:05:47 by fconde-p         ###   ########.fr       */
+/*   Updated: 2026/09/16 21:30:26 by fconde-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,11 +25,25 @@ static void	free_sphere(t_scene *scene)
 	}
 }
 
+static void	free_plain(t_scene *scene)
+{
+	t_plain	*temp;
+
+	temp = NULL;
+	while (scene->plain != NULL)
+	{
+		temp = scene->plain;
+		scene->plain = scene->plain->next;
+		free(temp);
+	}
+}
+
 int	free_scene(t_scene	*scene)
 {
 	if (scene != NULL)
 	{
 		free_sphere(scene);
+		free_plain(scene);
 	}
 	return (EXIT_SUCCESS);
 }
